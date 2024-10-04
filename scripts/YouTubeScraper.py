bce22836ecdffc280bc9ff9ed3f5dfb90321e2cc
soniaSherif <sonia.sherif@mnsu.edu>
2024-10-04 10:39:48 -0500

    Added data to SoniaScraped.csv

--- scripts/YouTubeScraper.py
+++ scripts/YouTubeScraper.py
@@ -7,7 +7,7 @@ import csv
 from googleapiclient.discovery import build
 
 # API key for YouTube Data API v3.
-API_KEY = '<API_KEY>'
+API_KEY = 'AIzaSyDjd3rj8rf2VRfFa5IcD0FP7nfKN4CbZZ0'
 
 # Function to convert a list of comments to csv.
 def comments_to_csv(comments: list, filename: str):
@@ -21,13 +21,13 @@ def comments_to_csv(comments: list, filename: str):
         print(f"Creating directory: {dir}")
         os.makedirs(dir)
     print(f"Writing {len(comments)} comments to {filename}")
-    with open(filename, 'a', newline='') as file:
+    with open(filename, 'a', newline='', encoding='utf-8') as file:
             writer = csv.writer(file)
             for comment in comments:
                 writer.writerow([comment])
 
 # Function to get video ids from a video name query.
-def get_video_ids(youtube, query: str, max_results=1):
+def get_video_ids(youtube, query: str, max_results=5):
     """
     Get video ids from a video name query.
     :param youtube: YouTube API object.
@@ -83,6 +83,13 @@ def search_comments_by_name(query: str, max_results=100):
 
 if __name__ == "__main__":
     # You can search by video name, or video id directly.
-    results = search_comments_by_name("Video Name", 10)
+    results = search_comments_by_name("Racist Karens KNOCKED OUT COLD And Getting Instant Karma! | BIG KARMA", 500)
+    results = search_comments_by_name("Full Debate: Biden and Trump in the First 2024 Presidential Debate | WSJ", 1000)
+    results = search_comments_by_name("\"You're Glorifying Obesity!\" Piers Morgan Interviews TikTok 'Fat Influencer'", 1000)
+    results = search_comments_by_name("RiceGum - Its EveryNight Sis feat. Alissa Violet (Official Music Video)", 1000)
+    results = search_comments_by_name("Worst Youtuber Apology Ever", 1000)
+    results = search_comments_by_name("Konstantin Kisin: Woke Culture HAS Gone Too Far - 7/8 | Oxford Union", 1000)
+    
+
     comments_to_csv(results, "./data/pathToCSV.csv")
     
